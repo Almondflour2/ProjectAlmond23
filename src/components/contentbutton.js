31b78307856a824239f69bcc507eee17f736f8aa
Almond 2 <almondflour2@gmail.com>
2023-02-26 18:46:11 -0500

    publish

--- src/components/contentbutton.js
+++ src/components/contentbutton.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import { topicH3, content } from './layout.module.css'
 
-//reusable button for content
+//reusable button for content. 
 const Content = ({ topic, children }) => {
-    const [visible, setVisible] = React.useState(false);
+    // declares a new state variable called 'visible' and gives it a value of false
+    const [visible, setVisible] = React.useState(false); 
+    // the H3 is given an onClick event which calls 'visible' and sets it to true with setstateaction
+    // the div's style is altered by the value of 'visible'. the ternary asks, is visible true? and sets it to 'block' or 'none'.
     return (
         <div className={content}>
-                <h3 className={topicH3} onClick={() => setVisible(!visible)}>{topic}</h3>
+                <button><div className={topicH3} onClick={() => setVisible(!visible)}><p>{topic}</p><p style={{display: visible ? 'none' : 'inline-block'}}>+</p><p style={{display: visible ? 'inline-block' : 'none'}}>-</p></div></button>
                 <div style={{display: visible ? 'block' : 'none'}}>{children}</div>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,29 @@ import Content from '../components/contentbutton.js'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
+    
     return (
         <Layout popcorn="ProjectAlmond23">
             <h3>SPRING 2023</h3>
             <h1>PORTFOLIO SITE BLUEPRINT</h1>
             <h1>Isabella Lawson</h1>
-            <p>Mission + Features + Timeline + Sitemap Design</p>
-            <Content topic="Mission +">
+            {/* <p><button onClick={() => useTargetButtonRef(mission).current.click()}>MISSION</button> + Features + Timeline + Sitemap Design</p> */}
+
+            <Content id="mission" topic="Mission">
                 <p>Learning developing has involved lots of trial-and-error, medium scale project building. This has gotten me tangible results that I can deploy and show people on the fly. However, now that I can breathe under the infinite loop water, I want to shift my focus into making cleaner code.</p>
                 <p>The outcome of this project will be a modular portfolio website. Modular, meaning I can integrate new features with ease. This will make discrepancies between new features and my base website simple to solve.</p>
                 <p>Thinking towards the future, when working with others I want my code to be easy to read and to debug. The thought process is, that if I can write and understand what my own code is, I can read others’ code as well. In the name of learning and retaining, keeping my code well commented and using modules with exports and imports will keep me on track to understand things fully.</p> 
-                <p>The internet is a scary, overstimulating, and intimidating place, so this practice of clean web-dev helps me extract the wonderful parts of it.</p>
+                <p>Project Almond 23 will be a website made with Gatsby like this one!</p>
             </Content>
-            <Content topic="Features +">
+            <Content topic="Features">
+                <p>This project will be a portfolio website I can show people on the fly.  It works well in place of a resume for a web developer. Along with a blog that logs my learning process and discovering new technologies, I am going to create a greeting card generator. My grandma suggested it!</p>
+                <p>The greeting card generator will be based on a meme-maker tutorial - the bones of this that I'm working with are the live text editing and the download to a png. A greeting card will comprise of an image background & optional text. To get the images - I will use a mix of my own API that stores some assets I hand select and an unsplash API.</p>
                 <p>Essential:</p>
                 <ul>
                     <li>Develop using React</li>
                     <li>Use MongoDB to store # content</li>
                     <li>Dynamically rendered # content - arrays/objects/sets/maps</li>
-                    <li>Use an API for something...</li>
+                    <li>Use an API to get images for the Greeting Card Generator.</li>
                 </ul>
                 <p>Supplemental:</p>
                 <ul>
@@ -32,17 +36,19 @@ const IndexPage = () => {
                     <li>Interactive UI features for the gcgen</li>
                 </ul>
             </Content>
-            <Content topic="Timeline +">
+            <Content topic="Timeline">
+            <p>I will get the bones of the site working and then create the greeting card generator and the blog going.</p>
             <p>Progress chunks:</p>
             <ul>
                 <li>Write content for site</li>
                 <li>Create hompeage design structure</li>
                 <li>Color scheme</li>
                 <li>Readme</li>
+                <li>Blog</li>
                 <li>Greeting Card Generator</li>
             </ul>
             </Content>
-            <Content topic="Sitemap +">
+            <Content topic="Sitemap">
             <p>Sitemap Design +</p>
             <StaticImage alt="sitemap mockup" src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c2f6e3bc-baf6-4f8e-81e3-b50e84719d8c/Portfolio_Sitemap.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230222%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230222T161048Z&X-Amz-Expires=86400&X-Amz-Signature=c198ecaf9f6b0eaf3036d31fa11f4e52a4b32764d2858e81f072f9788dd7a275&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Portfolio%2520Sitemap.png%22&x-id=GetObject"/>
             </Content>
